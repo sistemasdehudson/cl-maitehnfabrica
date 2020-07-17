ffebcc458a3e716d2aa8959ce85ee825f6f0e71b
Ubuntu <ubuntu@ip-172-31-30-126.us-east-2.compute.internal>
2020-07-17 16:44:57 +0000

    quitar saas

--- test13_default/__manifest__.py
+++ test13_default/__manifest__.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-#    Copyright (C) 2018  jeo Software  (http://www.jeosoft.com.ar)
+#    Copyright (C) 2020  jeo Software  (http://www.jeosoft.com.ar)
 #    All Rights Reserved.
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -41,7 +41,6 @@
 
     'git-repos': [
         'https://github.com/jobiols/cl-test.git',
-        'git@github.com:jobiols/saas.git'
 
     ],
     # list of images to use in the form 'name image-url'
